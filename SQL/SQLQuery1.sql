46c520114d268abf0cb0e209cd92aad507c34add
Hüseyin Aslan <91592183+HsynAslan@users.noreply.github.com>
2022-08-22 12:46:34 +0300

    Add files via upload

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -20,3 +20,34 @@ select * from products where UnitPrice between 10 and 46 order by UnitPrice
 
 Select * from products where CategoryID in (1,2)
 
+Select count(*) as [Urun Sayisi] from Products
+Select count(ProductName) from Products
+Select count(*) from Customers
+
+select Min(UnitPrice) from Products
+select Max(UnitPrice) from Products
+select Avg(UnitPrice) from Products
+select Sum(UnitPrice* Quantity) as Kazanc from [Order Details]
+select rand()
+
+select left('Huseyin Aslan', 5)			 
+select left(ProductName,3) from Products
+-- right, len, lower, upper
+
+select trim('	Huseyin Aslan	') -- bas ve sondaki spacelari atiyor
+select trim(ProductName) from Products where trim(ProductName) = 'Chai'
+-- rtrim sagdaki boslugu atmaya yarar
+-- ltrim soldaki boslugu atmaya yarar
+
+select reverse('Huseyin Aslan')
+
+select CHARINDEX('As','Huseyin Aslan',3) -- 3 dersek 'seyin Aslan' dan aramaya basliyacak 
+select ProductName from Products where CHARINDEX(' ', ProductName,1)>0
+
+select REPLACE('Huseyin Aslan',' ','_')
+
+select SUBSTRING('Huseyin Aslan',1,5)
+
+select ASCII('A')
+select char(65)
+
